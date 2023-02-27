43ed3881020e92259e4400b806516af6d28ecf8e
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-02-27 15:50:04 +0200

    update 01

--- src/index.js
+++ src/index.js
@@ -5,3 +5,17 @@ const DEBOUNCE_DELAY = 300;
 
 //debounce()
 //Notify.info("hello world!");
+/*
+
+https://restcountries.com/#filter-response
+
+Name
+
+Search by country name. It can be the native name or partial name
+
+https://restcountries.com/v2/name/{name}
+
+https://restcountries.com/v2/name/peru
+
+https://restcountries.com/v2/name/united
+*/
