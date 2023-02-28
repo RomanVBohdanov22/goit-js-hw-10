52c795d1c5571c453d2c2801fdffa43e00b21be9
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-02-28 11:42:29 +0200

    fff

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 var debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
 
+
+const inputLnk = document.querySelector("#search-box");
+const countryUlistLnk = document.querySelector(".country-list");
+const countryInfoLnk = document.querySelector(".country-info");
 //debounce()
 //Notify.info("hello world!");
 /*
